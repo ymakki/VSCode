c77a24228b06682404302d7acc6fa64c4912554d
ymakki <ymakki0824@gmail.com>
2024-04-12 23:45:13 +0900

    progate

--- test_java/src/Main.java
+++ test_java/src/Main.java
@@ -1,5 +1,7 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
-    Person.hello();
+    // printDataメソッドとfullNameメソッドの呼び出しを書き換えてください
+    Person.printData(Person.fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    Person.printData(Person.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
   }
 }
